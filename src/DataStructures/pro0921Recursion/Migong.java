0cc564a4bbe047d928f4f8e0c5f8932a989ff846
DreamerYee <1584802414@qq.com>
2019-09-21 19:14:45 +0800

    update Migong

--- src/DataStructures/pro0921Recursion/Migong.java
+++ src/DataStructures/pro0921Recursion/Migong.java
@@ -29,6 +29,7 @@ public class Migong {
         //map[2][2]=1;
 
 
+
         //输出地图
         System.out.println("地图的情况：");
         for(int i=0;i<8;i++){
@@ -39,7 +40,8 @@ public class Migong {
         }
 
         //使用递归回溯给小球找路
-        setWay(map,1,1);
+        //setWay(map,1,1);
+        setWay2(map,1,1);
 
         //输出新的地图，小球走过并标识过的地图
         System.out.println("小球走过并标识过的地图：");
@@ -49,6 +51,8 @@ public class Migong {
             }
             System.out.println();
         }
+
+
     }
 
     //递归回溯来给小球找路
@@ -57,7 +61,6 @@ public class Migong {
      * 如果小球能到map[6][5]位置，说明通路找到
      * 约定：当map[i][j]为0时表示该点没有走过，1表示墙，2表示通路可以走，3表示该点已走过，但是走不通
      * 在走迷宫时，需要确定一个策略：下->右->上->左,如果该点走不通，回溯
-     *
      *
      * @param map   表示地图
      * @param i     从哪个位置开始找
@@ -78,6 +81,33 @@ public class Migong {
                 }else if(setWay(map,i-1,j)){//向上走
                     return true;
                 }else if(setWay(map,i,j-1)){//左走
+                    return true;
+                }else{
+                    //说明该点走不通，是死路
+                    map[i][j]=3;
+                    return false;
+                }
+            }else {//如果map[i][j]!=0  可能为1,2,3
+                return false;
+            }
+        }
+    }
+
+    //修改找路的策略：上 右  下  左
+    public static boolean setWay2(int[][] map,int i,int j){
+        if(map[6][5]==2){//通路已经找到ok
+            return true;
+        }else {
+            if(map[i][j]==0){//如果当前该点没有走过
+                //按照策略下->右->上->左走
+                map[i][j]=2;//假定该点可以走通
+                if(setWay2(map,i-1,j)){//向上走
+                    return true;
+                }else if(setWay2(map,i,j+1)) {//向右走
+                    return true;
+                }else if(setWay2(map,i+1,j)){//向下走
+                    return true;
+                }else if(setWay2(map,i,j-1)){//左走
                     return true;
                 }else{
                     //说明该点走不通，是死路
