3771753efb8229564e065ee2728b8dec8a79a052
DreamerYee <1584802414@qq.com>
2019-09-22 20:46:42 +0800

    update BubbleSort

--- src/DataStructures/pro0922Sort/BubbleSort.java
+++ src/DataStructures/pro0922Sort/BubbleSort.java
@@ -1,6 +1,7 @@
 package DataStructures.pro0922Sort;
 
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @Author DreamYee
@@ -8,25 +9,37 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr={3,9,-1,10,-2};
+       // int[] arr={3,9,-1,10,-2};
 
         //容易理解，展示冒泡排序过程
 
-        //冒泡排序：时间复杂度：O(n^2)
+        /*
+        //测试冒泡排序
+        System.out.println("排序前");
+        System.out.println(Arrays.toString(arr));
+        */
 
-        int temp=0;//临时变量
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-1-i;j++){
-                //如果前面的数大于后面的就交换
-                if(arr[j]>arr[j+1]){
-                    temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
-            }
-            System.out.println("第"+(i+1)+"趟排序后的数组：");
-            System.out.println(Arrays.toString(arr));
+        //测试冒泡排序的速度O(n^2)，给80000个数据，测试
+        //创建要给80000个数据的数组
+        int[] arr=new int[80000];
+        for(int i=0;i<80000;i++){
+            arr[i]=(int)(Math.random()*8000000);
         }
+
+        Date date1=new Date();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str=simpleDateFormat.format(date1);
+        System.out.println("排序前的时间："+date1Str);
+
+        bubbleSort(arr);
+
+        Date date2=new Date();
+        String date2Str=simpleDateFormat.format(date2);
+        System.out.println("排序后的时间："+date2Str);
+
+
+        //System.out.println("排序后");
+        //System.out.println(Arrays.toString(arr));
 
         /*
         //2.第二趟：把第二大的数排在倒数第二位
@@ -65,5 +78,32 @@ public class BubbleSort {
         System.out.println("第四趟排序后的数组：");
         System.out.println(Arrays.toString(arr));
         */
+    }
+
+    //将前面的冒泡排序封装为一个方法
+    public static void bubbleSort(int[] arr){
+        //冒泡排序：时间复杂度：O(n^2)
+        boolean flag=false;//标识变量，是否进行过交换
+        int temp=0;//临时变量
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                //如果前面的数大于后面的就交换
+                if(arr[j]>arr[j+1]){
+                    flag=true;
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+            //System.out.println("第"+(i+1)+"趟排序后的数组：");
+            //System.out.println(Arrays.toString(arr));
+
+            if(flag==false){//说明在一趟排序中，一次交换都没有发生过
+                break;
+            }else {
+                flag=false;//重置flag，进行下次的判断
+            }
+
+        }
     }
 }
