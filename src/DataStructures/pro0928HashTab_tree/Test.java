b41da16e080a3e94da145fabe835f56a6af924e9
DreamerYee <1584802414@qq.com>
2019-09-28 10:07:45 +0800

    update ArrayList

---
+++ src/DataStructures/pro0928HashTab_tree/Test.java
@@ -0,0 +1,22 @@
+package DataStructures.pro0928HashTab_tree;
+
+import java.util.ArrayList;
+
+/**
+ * @Author DreamYee
+ * @Create 2019/09/28  9:29
+ */
+public class Test {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        //以ArrayList为例，看看是怎样扩容的
+        @SuppressWarnings("rawtypes")
+        //ArrayList维护了数组transient Object[] elementData;
+        /**
+         * ArrayList底层仍然是进行数组扩容
+         */
+        ArrayList arrayList=new ArrayList();
+
+
+    }
+}
