3944b45bf8a010f38b93a5383cc3a14a6bf7fb96
DreamerYee <1584802414@qq.com>
2019-10-01 19:58:04 +0800

    update 存储二叉树

--- src/DataStructures/pro1001binaryTree/ArrBinaryTreeDemo.java
+++ src/DataStructures/pro1001binaryTree/ArrBinaryTreeDemo.java
@@ -10,15 +10,17 @@ public class ArrBinaryTreeDemo {
         //创建一个ArrayBinaryTree
         ArrBinaryTree arrBinaryTree=new ArrBinaryTree(arr);
         arrBinaryTree.preOrder(0);//1,2,4,5,3,6,7
+        arrBinaryTree.infixOrder(6);//4,2,5,1,6,3,7
+        arrBinaryTree.postOrder(3);//4,5,2,6,7,3,1
     }
 }
 
 //编写一个ArrayBinaryTree类，实现顺序存储二叉树遍历
-class ArrBinaryTree{
+class ArrBinaryTree {
     private int[] arr;
 
-    public ArrBinaryTree(int[] arr){
-        this.arr=arr;
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
     }
 
     /*
@@ -31,15 +33,59 @@ class ArrBinaryTree{
     //编写一个方法，完成顺序存储二叉树的前序遍历
 
     /**
-     * @param index   数组的下标
+     * @param index 数组的下标
      */
-    public void preOrder(int index){
+    public void preOrder(int index) {
         //如果数组为空，或者arr.length=0
-        if(arr==null||arr.length==0){
+        if (arr == null || arr.length == 0) {
             System.out.println("数组为空，不能按照二叉树的前序遍历");
         }
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+            //输出当前这个元素
+            System.out.print(arr[index]+"\t");
+            //向右递归遍历
+            if ((index * 2 + 2) < arr.length) {
+                preOrder(2 * index + 2);
+            }
+        }
+    }
+
+    //编写一个方法，完成顺序存储二叉树的中序遍历
+
+    /**
+     * @param index 数组的下标
+     */
+    public void infixOrder(int index) {
+        //如果数组为空，或者arr.length=0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的中序遍历");
+        }
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
         //输出当前这个元素
-        System.out.println(arr[index]);
+        System.out.print(arr[index]+"\t");
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+    }
+
+
+    //编写一个方法，完成顺序存储二叉树的后序遍历
+
+    /**
+     * @param index   数组的下标
+     */
+    public void postOrder(int index){
+        //如果数组为空，或者arr.length=0
+        if(arr==null||arr.length==0){
+            System.out.println("数组为空，不能按照二叉树的后序遍历");
+        }
+
         //向左递归遍历
         if((index*2+1)<arr.length){
             preOrder(2*index+1);
@@ -48,5 +94,7 @@ class ArrBinaryTree{
         if((index*2+2)<arr.length){
             preOrder(2*index+2);
         }
+        //输出当前这个元素
+        System.out.print(arr[index]+"\t");
     }
 }
