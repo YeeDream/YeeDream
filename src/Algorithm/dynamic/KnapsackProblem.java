fb641e9ff10ba63a6b007b4c3b9fe02f2dc58ee0
DreamerYee <1584802414@qq.com>
2019-10-19 09:57:39 +0800

    update 动态规划

--- src/Algorithm/dynamic/KnapsackProblem.java
+++ src/Algorithm/dynamic/KnapsackProblem.java
@@ -12,7 +12,7 @@ public class KnapsackProblem {
         int n=val.length;//物品的个数
 
         //为了记录放入商品的情况，定一个二维数组
-        int[][] path=new int[][];
+        int[][] path=new int[n+1][m+1];
 
         //创建二维数组
         //v[i][j]
@@ -35,7 +35,13 @@ public class KnapsackProblem {
                 }else {
                    // v[i][j]=Math.max(v[i-1][j],val[i-1]+v[i-1][j-w[i-1]]);
                     //为了记录商品存放到背包的情况，我们不能简单地使用上面的公式，使用if-else
-                    if()
+                    if(v[i-1][j]<val[i-1]+v[i-1][j-w[i-1]]){
+                        v[i][j]=val[i-1]+v[i-1][j-w[i-1]];
+                        //把当前的记录记录到path
+                        path[i][j]=1;
+                    }else {
+                        v[i][j]=v[i-1][j];
+                    }
                 }
             }
         }
@@ -46,6 +52,28 @@ public class KnapsackProblem {
                 System.out.print(v[i][j]+" ");
             }
             System.out.println();
+        }
+        System.out.println("==============");
+        //输出最后放入的商品
+        //遍历path
+        /*
+        for(int i=0;i<path.length;i++){
+            for(int j=0;j<path[i].length;j++){
+                if(path[i][j]==1){
+                    System.out.printf("第%d个商品放入到背包\n",i);
+                }
+            }
+        }
+        */
+
+        int i=path.length-1;
+        int j=path[i].length-1;
+        while (i>0){//从后往前
+            if(path[i][j]==1) {
+                System.out.printf("第%d个商品放入到背包\n", i);
+                j -= w[i - 1];
+            }
+            i--;
         }
     }
 
