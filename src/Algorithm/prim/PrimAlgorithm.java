ce93f41447366ac897e6ce54a4d8a21e561ecab4
DreamerYee <1584802414@qq.com>
2019-10-21 17:02:32 +0800

    update prim算法

--- src/Algorithm/prim/PrimAlgorithm.java
+++ src/Algorithm/prim/PrimAlgorithm.java
@@ -1,6 +1,5 @@
 package Algorithm.prim;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -29,33 +28,35 @@ public class PrimAlgorithm {
         minTree.createGraph(graph,verxs,data,weight);
         //输出
         minTree.showGraph(graph);
+
+        //
+        minTree.prim(graph,0);
     }
 }
 
 //创建最小生成树
-class MinTree{
+class MinTree {
     //创建图的邻接矩阵
 
     /**
-     *
-     * @param graph 图对象
-     * @param verxs 图对应的顶点个数
-     * @param data  图的各个顶点的值
-     * @param weight  图的邻接矩阵
+     * @param graph  图对象
+     * @param verxs  图对应的顶点个数
+     * @param data   图的各个顶点的值
+     * @param weight 图的邻接矩阵
      */
-    public void createGraph(MGraph graph,int verxs,char[] data,int[][] weight){
-        int i,j;
-        for(i=0;i<verxs;i++){//遍历顶点
-            graph.data[i]=data[i];
-            for(j=0;j<verxs;j++){
-                graph.weight[i][j]=weight[i][j];
+    public void createGraph(MGraph graph, int verxs, char[] data, int[][] weight) {
+        int i, j;
+        for (i = 0; i < verxs; i++) {//遍历顶点
+            graph.data[i] = data[i];
+            for (j = 0; j < verxs; j++) {
+                graph.weight[i][j] = weight[i][j];
             }
         }
     }
 
     //显示图的邻接矩阵
-    public void showGraph(MGraph graph){
-        for (int[] link:graph.weight){
+    public void showGraph(MGraph graph) {
+        for (int[] link : graph.weight) {
             System.out.println(Arrays.toString(link));
         }
     }
@@ -63,15 +64,46 @@ class MinTree{
     //编写prim算法，得到最小生成树
 
     /**
-     *
-     * @param graph   图
-     * @param v  表示从图的第几个顶点开始生成
+     * @param graph 图
+     * @param v     表示从图的第几个顶点开始生成
      */
-    public void prim(MGraph graph,int v){
+    public void prim(MGraph graph, int v) {
         //visited[]标记顶点是否被访问过
         int visited[] = new int[graph.verxs];
         //visited[]默认为0，没有访问过
-        for
+        /*
+        for(int i=0;i<graph.verxs;i++){
+            visited[i]=0;
+        }
+        */
+
+        //把当前节点标记为已访问
+        visited[v] = 1;
+        //h1与h2记录两个定点的下标
+        int h1 = -1;
+        int h2 = -1;
+        int minweight = 10000;//将minweight初始化为一个大值，后在遍历过程中会替换
+        for (int k = 0; k < graph.verxs; k++) {
+            {//因为有 graph.verxs顶点，普利姆算法结束后，有 graph.verxs-1边
+                //这个是确定每一次生成的子图 ，和哪个结点的距离最近
+                for (int i = 0; i < graph.verxs; i++) {// i结点表示被访问过的结点
+                    for (int j = 0; j < graph.verxs; j++) {//j结点表示还没有访问过的结点
+                        if (visited[i] == 1 && visited[j] == 0 && graph.weight[i][j] < minweight) {
+                            //替换minWeight(寻找已经访问过的结点和未访问过的结点间的权值最小的边)
+                            minweight = graph.weight[i][j];
+                            h1 = i;
+                            h2 = j;
+                        }
+                    }
+                }
+            }
+            //找到一条边最小
+            System.out.println("边<"+graph.data[h1]+","+graph.data[h2]+">权值："+minweight);
+            //将当前这个结点标记为已经访问
+            visited[h2]=1;
+            //minWeight 重新设置为最大值 10000
+            minweight=10000;
+        }
     }
 }
 
