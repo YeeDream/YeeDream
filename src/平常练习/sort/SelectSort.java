411461bebe033239297ddaa08c59ef4864868c0b
DreamerYee <1584802414@qq.com>
2019-10-21 23:30:28 +0800

    update 排序算法——选择排序

--- src/平常练习/sort/SelectSort.java
+++ src/平常练习/sort/SelectSort.java
@@ -1,12 +1,16 @@
 package 平常练习.sort;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @Author DreamYee
  * @Create 2019/10/21  19:34
  */
 public class SelectSort {
     public static void main(String[] args) {
+        /*
         int[] array=new int[SIZE];
         int i;
         for(i=0;i<SIZE;i++){
@@ -24,6 +28,24 @@ public class SelectSort {
         for(i=0;i<SIZE;i++){
             System.out.print(array[i]+" ");
         }
+        */
+
+        //测试效率
+        int[] array=new int[80000];
+        for(int i=0;i<80000;i++){
+            array[i]=(int)(Math.random()*8000000);//生成一个[0,8000000)数
+        }
+
+        Date date1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(date1);
+        System.out.println("排序前的时间：" + date1Str);
+
+        selectSort(array);
+
+        Date date2 = new Date();
+        String date2Str = simpleDateFormat.format(date2);
+        System.out.println("排序后的时间：" + date2Str);
     }
 
     static final int SIZE=5;
@@ -42,11 +64,13 @@ public class SelectSort {
                 a[i]=a[index];
                 a[index]=temp;
             }
+            /*
             System.out.print("第"+i+"步排序结果：");//输出每步排序的结果
             for(int h=0;h<a.length;h++){
                 System.out.print(" "+a[h]); //输出
             }
             System.out.println();
+            */
         }
     }
 }
