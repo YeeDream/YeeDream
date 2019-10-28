20056a42bf6dcd1be756f641bc61b9579bc800b2
DreamerYee <1584802414@qq.com>
2019-10-28 21:08:19 +0800

    initial commit

---
+++ src/thread/WaitNotifyInterupt.java
@@ -0,0 +1,31 @@
+package thread;
+
+/**
+ * @Author DreamYee
+ * @Create 2019/10/28  18:31
+ */
+public class WaitNotifyInterupt {
+    static Object obj=new Object();
+
+    public static void main(String[] args) throws InterruptedException{
+        //创建线程
+        Thread threadA=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    System.out.println("---begin---");
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        threadA.start();
+
+        Thread.sleep(1000);
+
+        System.out.println("---begin interrupt threadA---");
+        threadA.interrupt();
+        System.out.println("---end interrupt threadA---");
+    }
+}
