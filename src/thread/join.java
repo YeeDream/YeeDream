8305d230218c3f4bcaa116429ce1437e5d12c75c
DreamerYee <1584802414@qq.com>
2019-10-29 17:53:26 +0800

    update join()函数

---
+++ src/thread/join.java
@@ -0,0 +1,44 @@
+package thread;
+
+/**
+ * @Author DreamYee
+ * @Create 2019/10/29  17:41
+ */
+public class join {
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadOne = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(1000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("child threadOne over!");
+            }
+        });
+
+        Thread threadTwo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(1000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("child threadTwo over");
+            }
+        });
+
+        //启动子线程
+        threadOne.start();
+        threadTwo.start();
+
+        System.out.println("wait all child thread over!");
+
+        //等待子线程执行完毕，返回
+        threadOne.join();
+        threadTwo.join();
+        System.out.println("all child thread over!");
+    }
+}
