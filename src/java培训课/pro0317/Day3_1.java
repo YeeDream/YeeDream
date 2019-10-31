316167d8b591a6955ecba746de26433fc5450ee2
DreamerYee <1584802414@qq.com>
2019-10-31 22:12:05 +0800

    update ThreadLocal

---
+++ src/java培训课/pro0317/Day3_1.java
@@ -0,0 +1,17 @@
+package java培训课.pro0317;
+
+public class Day3_1 {
+    public static void main(String[] args) {
+        String str = "设计费";
+        System.out.println(str);
+
+        //三要素：类型 名称 值
+        long m;   //声明了一个变量m,类型为long
+
+        /*
+        变量必须赋值 如果不赋值则无法使用
+         */
+        m = 88;   //初始化  第一次赋值
+        System.out.println(m);
+    }
+}
